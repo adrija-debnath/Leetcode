d31aaf5d7bfdabdd48f23f52e4b7a727c41d00be
ADRIJA DEBNATH <114132671+adrija-debnath@users.noreply.github.com>
2024-07-15 00:20:32 +0530

    Time: 12 ms (89.57%), Space: 27 MB (37.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int ans;
+      int count=0;
+      for (const int num: nums){
+      if (count==0){
+      ans=num;
+      }
+      count+=(ans==num)?1:-1;
+      }
+      return ans;
+    }
+};
